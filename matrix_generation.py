96cb7d37e85c58eeeb314eba54b6edc52c28c47c
Stoupy51 <stoupy51@gmail.com>
2024-02-17 10:42:33 +0100

    Commit devops TD1 Unit tests + reinforcements

--- matrix_generation.py
+++ matrix_generation.py
@@ -36,8 +36,122 @@ def userinput_matrix(dim: list = None) -> list:
 		dimensions = dim
 	for i in range(int(dimensions[0])):
 		if len(dimensions) > 1:
-			matrix.append(userinput_matrix())
+			matrix.append(userinput_matrix(dimensions[1:]))
 		else:
 			matrix.append(float(input(f"Enter the value for the element at position {i}: ")))
 	return matrix
+
+
+# Unit test functions
+COLOR_GREEN = "\033[92m"
+COLOR_RED = "\033[91m"
+COLOR_RESET = "\033[0m"
+import traceback
+
+def test_generate_matrix() -> bool:
+	""" Unit test for the generate_matrix function.	"""
+	try:
+		# Try to generate a matrix of size 3*3*3 with values between 0.5 and 1
+		sizes = [3,3,3]
+		matrix = generate_matrix(sizes, 1, 2, 2.0)
+
+		# Check if the matrix has the right size for first dimension
+		assert len(matrix) == sizes[0], f"Matrix has wrong size for first dimension: {len(matrix)} instead of {sizes[0]}"
+
+		# For each element in the first dimension
+		for i in range(len(matrix)):
+			
+			# Check if the matrix has the right size for second dimension
+			assert len(matrix[i]) == sizes[1], f"Matrix has wrong size for second dimension: {len(matrix[i])} instead of {sizes[1]}"
+
+			# For each element in the second dimension
+			for j in range(len(matrix[i])):
+
+				# Check if the matrix has the right size for third dimension
+				assert len(matrix[i][j]) == sizes[2], f"Matrix has wrong size for third dimension: {len(matrix[i][j])} instead of {sizes[2]}"
+
+				# For each element in the third dimension
+				for k in range(len(matrix[i][j])):
+					
+					# Check if the element is between 0.5 and 1
+					assert 0.5 <= matrix[i][j][k] <= 1, f"Element at position {i},{j},{k} is not between 0.5 and 1: {matrix[i][j][k]}"
+
+	except Exception:
+		print(f"{COLOR_RED}Error in test_generate_matrix: ", traceback.format_exc(), f"{COLOR_RESET}\n")
+		return False
+
+	# Everythin's fine
+	return True
+
+def test_userinput_matrix() -> bool:
+	""" Unit test for the userinput_matrix function.	"""
+	try:
+		# Disable stdout temporary
+		import sys
+		temporary_out = "test_output.temp"
+		temporary_in = "test_input.temp"
+		sys.stdout = open(temporary_out, "w")
+
+		# Try to generate a matrix of size 4*4 with user input
+		to_stdin = "4x4 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16".replace(" ", "\n")
+		sys.stdin = open(temporary_in, "w")
+		sys.stdin.write(to_stdin)
+		sys.stdin.close()
+		sys.stdin = open(temporary_in, "r")
+		matrix = userinput_matrix()
+		sys.stdin.close()
+		sys.stdin = sys.__stdin__
+
+		# Check if the matrix has the right size for first dimension
+		assert len(matrix) == 4, f"Matrix has wrong size for first dimension: {len(matrix)} instead of 4"
+
+		# For each element in the first dimension
+		for i in range(len(matrix)):
+			
+			# Check if the matrix has the right size for second dimension
+			assert len(matrix[i]) == 4, f"Matrix has wrong size for second dimension: {len(matrix[i])} instead of 4"
+
+			# For each element in the second dimension
+			for j in range(len(matrix[i])):
+					
+				# Check if the element is the same as the input
+				assert matrix[i][j] == (i*4+j+1), f"Element at position {i},{j} is not the same as the input: {matrix[i][j]}"
+
+	except Exception:
+		# Go back to stdout
+		sys.stdout.close()
+		sys.stdout = sys.__stdout__
+
+		print(f"{COLOR_RED}Error in test_userinput_matrix: ", traceback.format_exc(), f"{COLOR_RESET}\n")
+		return False
+	finally:
+		# Go back to stdout
+		sys.stdout.close()
+		sys.stdout = sys.__stdout__
+
+		# Remove temporary files
+		import os
+		os.remove(temporary_out)
+		os.remove(temporary_in)
+
+ 	# Everythin's fine
+	return True
+
+
+
+# Run if directly called (not imported): runs all the tests
+if __name__ == "__main__":
+
+	# Run all the tests and remember the results
+	bools = []
+	bools.append(test_generate_matrix())
+	bools.append(test_userinput_matrix())
+
+	# Summary
+	print("Summary:")
+	for i in range(len(bools)):
+		if bools[i]:
+			print(f"{COLOR_GREEN}Test {i} passed!{COLOR_RESET}")
+		else:
+			print(f"{COLOR_RED}Test {i} failed!{COLOR_RESET}")
 
